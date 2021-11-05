69cd9e3c392328bbc9ef0996ecfc88fad6cdf8ff
lukasz ciepichal <lukaszciepichal1@gmail.com>
2021-11-05 13:04:49 +0100

    wyswietlanie opisu

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let paragraf = (document.getElementById('opis'));
+
+let button = document.querySelector('button');
+
+document.querySelector('button').onclick = function (){
+    opis.textContent = 'ala ma kota'
+}
+    
+    
